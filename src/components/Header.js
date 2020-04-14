3131eb077147f4e8a4c94d7a419c9b3a84c20f81
popeye <popeye@Cabernet.emz>
2020-04-14 14:00:34 -0500

    Setup Header with authentication (logout)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
 
-const Header = () => (
+// export this one for testing purposes:
+// destructure ({ startLogout })
+export const Header = ({ startLogout }) => (
   <header>
     <div>
       <h1>Expensify</h1>
       <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
       <NavLink to="/create" activeClassName="is-active">Add Expense</NavLink>
+      <button onClick={startLogout}>Logout</button>
     </div>
   </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+  startLogout: () => dispatch(startLogout())
+});
+
+// connected version of Header:
+// mapStateToProps = undefined since we have no state to manipulate
+export default connect(undefined, mapDispatchToProps)(Header);
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
+
+// Each test will start with a 'clean' copy of the following:
+let startLogout, wrapper;
+
+beforeEach(() => {
+  startLogout = jest.fn();
+  wrapper = shallow(<Header startLogout={startLogout}/>);
+});
 
 test('should render Header correctly', () => {
-  const wrapper = shallow(<Header />);
+  //const wrapper = shallow(<Header startLogout={() => {}}/>);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogout on button click', () => {
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
 });
